16c1800752f1c1b847f3ab6eac1ae5a139a4cdc9
mattiacoli <mattia.coli@aol.com>
2025-06-30 11:06:31 +0200

    Snack 1 Add custom hook useSwitch for toggling boolean state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
+import useSwitch from "./useSwitch"
 
 function App() {
-
+  const [isOn, toogle] = useSwitch()
 
   return (
     <>
-
-
+      <h1>Il valore è : {isOn ? "ON" : "OFF"}</h1>
+      <button onClick={toogle}>Cambia Stato</button>
     </>
   )
 }
